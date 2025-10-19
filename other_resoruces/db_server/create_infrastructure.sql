48655c075a61c3e79a3aba8bb22d625c775c5dbd
CommanderSpinner <klaus.kutulu@gmail.com>
2025-10-19 19:35:53 +0200

    not sure how to finnish this

--- other_resoruces/db_server/create_infrastructure.sql
+++ other_resoruces/db_server/create_infrastructure.sql
@@ -1,18 +1,27 @@
 
-CREATE TABLE user(
-    nid SERIAL PRIMARY KEY,
+CREATE TABLE "user"(
+    uid SERIAL PRIMARY KEY,
     user_name VARCHAR(100) UNIQUE NOT NULL,
     hashed_password TEXT NOT NULL
 );
 
-CREATE TABLE type(
+CREATE TABLE "type"(
     type VARCHAR(100) PRIMARY KEY UNIQUE NOT NULL,
     file_location TEXT NOT NULL,
-)
+);
+
+CREATE TABLE chat(
+    cid SERIAL PRIMARY KEY,
+    
+);
+
+CREATE TABLE user_chats(
+    
+);
 
 CREATE TABLE message(
     mid SERIAL PRIMARY KEY,
     created_at TIMESTAMP DEFAULT NOW(),
-    nid INT REFERENCES user(nid),
-    type VARCHAR(100) REFERENCES type(type),
-)
+    uid INT NOT NULL REFERENCES "user"(uid) ON DELETE CASCADE,
+    type VARCHAR(100) NOT NULL REFERENCES "type"(type) ON DELETE SET NULL
+);
