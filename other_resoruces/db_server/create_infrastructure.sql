8396649b968bb318f76fc7b0f8d19c6ae6b495bc
CommanderSpinner <klaus.kutulu@gmail.com>
2025-10-19 15:09:19 +0200

    half thru the db with sql script

--- other_resoruces/db_server/create_infrastructure.sql
+++ other_resoruces/db_server/create_infrastructure.sql
@@ -3,4 +3,16 @@ CREATE TABLE user(
     nid SERIAL PRIMARY KEY,
     user_name VARCHAR(100) UNIQUE NOT NULL,
     hashed_password TEXT NOT NULL
+);
+
+CREATE TABLE type(
+    type VARCHAR(100) PRIMARY KEY UNIQUE NOT NULL,
+    file_location TEXT NOT NULL,
+)
+
+CREATE TABLE message(
+    mid SERIAL PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT NOW(),
+    nid INT REFERENCES user(nid),
+    type VARCHAR(100) REFERENCES type(type),
 )
