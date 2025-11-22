574e25be44d240e87e0db68786104673e13118ca
CommanderSpinner <klaus.kutulu@gmail.com>
2025-11-22 04:42:33 +0100

    Update createdb.sh

--- server/files/server_data/db_server/createdb.sh
+++ server/files/server_data/db_server/createdb.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+docker compose up -d
+
+echo "ckeaning old db"
+
+docker exec -it postgres psql -U admin -d postgres -c "DROP DATABASE IF EXISTS rchats; CREATE DATABASE rchats;"
+
 echo "executing script to build db in postgres"
 
-psql -U admin -d rchats -f create_db.sql
+docker exec -it postgres psql -U admin -d rchats -f create_db.sql
