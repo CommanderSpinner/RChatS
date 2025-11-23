aeeda3464ddf45870a72997f2c053d1ea2d202bf
CommanderSpinner <klaus.kutulu@gmail.com>
2025-11-23 12:03:34 +0100

    cleaning only now if wanted

--- server/build_debug.sh
+++ server/build_debug.sh
@@ -2,20 +2,19 @@
 
 set -e
 
-echo "cleaning"
-cargo clean
+if [[ "$1" = "clean" ]]; then
+    echo "cleaning"
+    cargo clean
+fi
 
 echo "building"
 cargo build
 
 echo "coping files for post install and server to build dir"
-
 cp -R files ../target/debug/
 
 echo "executing script start db server"
-
 bash ../target/debug/files/server_data/db_server/createdb.sh
 
 echo "testing server"
-
 ../target/debug/server
