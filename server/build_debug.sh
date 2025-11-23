3ba7b9ea1bb1d999f693b1191351d963fdb48331
CommanderSpinner <klaus.kutulu@gmail.com>
2025-11-23 17:24:12 +0100

    fixed scripts and also only cleaning db now if specified via args
    lib.rs in main folder is required otherwise it doesnt compile

--- server/build_debug.sh
+++ server/build_debug.sh
@@ -14,7 +14,16 @@ echo "coping files for post install and server to build dir"
 cp -R files ../target/debug/
 
 echo "executing script start db server"
-bash ../target/debug/files/server_data/db_server/createdb.sh
+cd ../target/debug/files/server_data/db_server/
+pwd
+
+if [[ "$1" = "clean" ]]; then
+    bash createdb.sh clean
+else
+    bash createdb.sh
+fi
 
 echo "testing server"
-../target/debug/server
+cd ../../../
+pwd
+./server
--- server/files/server_data/db_server/createdb.sh
+++ server/files/server_data/db_server/createdb.sh
@@ -8,10 +8,13 @@ until docker exec postgres pg_isready -U admin -d postgres; do
     sleep 1
 done
 
-echo "Cleaning old db"
-docker exec postgres psql -U admin -d postgres -c "DROP DATABASE IF EXISTS rchats;"
-docker exec postgres psql -U admin -d postgres -c "CREATE DATABASE rchats;"
+if [[ "$1" = "clean" ]]; then
+    echo "Cleaning old db"
+    docker exec postgres psql -U admin -d postgres -c "DROP DATABASE IF EXISTS rchats;"
+    docker exec postgres psql -U admin -d postgres -c "CREATE DATABASE rchats;"
+    echo "Executing script to build db in postgres"
+    docker cp create_db.sql postgres:/create_db.sql
+    docker exec postgres psql -U admin -d rchats -f /create_db.sql
+fi
 
-echo "Executing script to build db in postgres"
-docker cp create_db.sql postgres:/create_db.sql
-docker exec postgres psql -U admin -d rchats -f /create_db.sql
+
