1de11733baf4ae4bc75618f50bc4454fe066f30a
CommanderSpinner <klaus.kutulu@gmail.com>
2025-12-17 20:03:05 +0100

    still need to test script and make copys to dirs but otherwise script looks good

--- server/scripts/build.ps1
+++ server/scripts/build.ps1
@@ -1,13 +1,40 @@
 
 <#
-"platform" specifies the build target
 "clean" decides if you want to cargo clean or just build again
 "package" if true packages the compiled bin into a zip file with everything needed in it an the other folders removed
 #>
+
 param(
-    [string]$platform,
-    [bool]$clean
+    [bool]$clean,
     [bool]$package
 )
 
-if ()
+cd ..
+$Server_root = Get-Location
+Write-Output "server root: $($Server_root)"
+
+if($clean) {
+    cargo clean
+}
+
+#check what platform
+
+$Platform = "x86_64-unknown-linux-gnu" #default is linux
+if ($PSVersionTable.Platform -eq 'Win32NT') {
+    $Platform = x86_64-pc-windows-msvc
+}
+rustup target add $Platform
+Write-Output "building for: $(Platform)"
+
+#check for debugg or release build
+if($package) {
+    cargo build --release --target $Platform
+} else {
+    cargo build --target $Platform
+}
+
+# still need to copy files to folders
+
+# return to scripts folder
+cd $server_root
+cd scripts
