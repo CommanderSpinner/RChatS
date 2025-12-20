823402df520e5815fa27904df30a5b4c9d1f21ff
CommanderSpinner <klaus.kutulu@gmail.com>
2025-12-20 11:26:19 +0000

    added parameter for platform also vars for the dirs that are important
    made documentation for this mess also

--- server/scripts/build.ps1
+++ server/scripts/build.ps1
@@ -1,3 +1,4 @@
+#!/usr/bin/env pwsh
 
 <#
 "clean" decides if you want to cargo clean or just build again
@@ -5,31 +6,58 @@
 #>
 
 param(
-    [bool]$clean,
-    [bool]$package
+    [switch]$clean,
+    [switch]$package,
+    [String]$platform="x86_64-unknown-linux-gnu"
 )
+
+$ErrorActionPreference = 'Stop'
+
+# Assign defaults if not provided
+if ($null -eq $clean) { $clean = $false }
+if ($null -eq $package) { $package = $false }
 
 cd ..
 $Server_root = Get-Location
+
+#getting "target" dir from cargo
+cd ..
+cd target
+$target_dir = Get-Location
+
+#getting dir of platform target
+mkdir $Platform
+cd $Platform
+if($package){
+    mkdir release
+    cd release
+} else {
+    mkdir debug
+    cd debug
+}
+
+$platform_binary = Get-Location
+
+#return to server root
+cd $Server_root
+
 Write-Output "server root: $($Server_root)"
+Write-Output "platform target dir: $($platform_binary)"
 
 if($clean) {
+    Write-Output "cleaning"
     cargo clean
 }
 
-#check what platform
-
-$Platform = "x86_64-unknown-linux-gnu" #default is linux
-if ($PSVersionTable.Platform -eq 'Win32NT') {
-    $Platform = x86_64-pc-windows-msvc
-}
 rustup target add $Platform
-Write-Output "building for: $(Platform)"
+Write-Output ("building for: {0}" -f $Platform)
 
 #check for debugg or release build
 if($package) {
+    Write-Output "release build"
     cargo build --release --target $Platform
 } else {
+    Write-Output "debug build"
     cargo build --target $Platform
 }
 
---
+++ server/scripts/scripts_doc.md
@@ -0,0 +1,4 @@
+# Scripts for building and packaging the software automated
+1. script is made for windows and linux 64 bit. if you need a diffrent platform write your own script contribute to these scripts or just do the steps manually 
+2. building with the powershell script is recomended
+WIP
