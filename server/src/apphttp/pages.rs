d75666db54c51d584a0ace520479c5566c2252d1
CommanderSpinner <klaus.kutulu@gmail.com>
2026-03-08 14:01:35 +0100

    in debug mode checking if web interface gets acessed

--- server/src/apphttp/pages.rs
+++ server/src/apphttp/pages.rs
@@ -1,5 +1,6 @@
 use askama::Template;
 use axum::response::Html;
+use tower_cookies::{Cookie, Cookies};
 
 #[derive(Template)]
 #[template(path = "index.html")]
@@ -11,6 +12,8 @@ pub async fn index() -> Html<String> {
     let page = IndexTemplate {
         title: "Login",
     };
+
+    common::debug_println!("web interface accessed");
 
     Html(page.render().unwrap())
 }
