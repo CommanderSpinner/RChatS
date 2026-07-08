3ebd1c4a04ef9b9accd6cd52a8d78c64aaf81396
CommanderSpinner <klaus.kutulu@gmail.com>
2026-07-08 10:36:31 +0200

    sending create chat request

--- server/templates/add_contacts.js
+++ server/templates/add_contacts.js
@@ -2,5 +2,12 @@ let btn_contacts = document.getElementById("add_contact");
 
 btn_contacts.addEventListener("click", function () {
   console.log("adding contact");
-  chat.send("dummy string");
+
+  const createChat = {
+    type: "CreateChat",
+    chatname: "My first chat",
+    userids: [1, 2]
+  };
+
+    chat.send(createChat);
 });
