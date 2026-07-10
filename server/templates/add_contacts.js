650dd3f309027bd8b6ac3678b4ecb0b577dba295
CommanderSpinner <klaus.kutulu@gmail.com>
2026-07-11 01:03:47 +0200

    add contacts sends now uids too backend when clicking button

--- server/templates/add_contacts.js
+++ server/templates/add_contacts.js
@@ -3,10 +3,12 @@ let btn_contacts = document.getElementById("add_contact");
 btn_contacts.addEventListener("click", function () {
   console.log("adding contact");
 
+  let uid1 = getCookie("userid");
+  let uid2 = prompt("Enter user ID of your contact");
   const createChat = {
     type: "CreateChat",
     chatname: "My first chat",
-    userids: [1, 2]
+    userids: [uid1, uid2]
   };
 
     chat.send(createChat);
